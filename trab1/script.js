db2b4cda1ca1253f5ed6787b08f3297df5f5b6ef
Rllucena <129807139+Rllucena@users.noreply.github.com>
2024-10-12 20:44:07 -0300

    Update script.js

--- trab1/script.js
+++ trab1/script.js
@@ -75,7 +75,7 @@ async function atualizarCartao() {
             imagemCartao.style.display = 'block';
         } else {
             imagemCartao.style.display = 'none';
-            alert('Não é uma imagem');  // Exibir alerta caso a URL não seja de uma imagem
+            imagemCartao.src = https://salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png;  // Exibir um image not found no lugar da imagem
         }
     } else {
         imagemCartao.style.display = 'none';
